a62608d5e3fb15df6537e10af577c9c9c610be4a
Matt Schmulen <matt.schmulen@gmail.com>
2013-11-09 12:24:39 -0800

    mockup

---
+++ ios-app/ios-app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ios-app
+//
+//  Created by Matt Schmulen on 11/9/13.
+//  Copyright (c) 2013 Matt Schmulen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ios-mockup/ios-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-app
+//
+//  Created by Matt Schmulen on 11/9/13.
+//  Copyright (c) 2013 Matt Schmulen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
