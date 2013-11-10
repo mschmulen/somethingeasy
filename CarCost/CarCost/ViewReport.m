a1b6d25639156680d5c9bd212fc433f0b4a6c385
Matt Schmulen <matt.schmulen@gmail.com>
2013-11-09 22:42:38 -0800

    report view update,  hacked together some of the art layouts

--- CarCost/CarCost/ViewReport.m
+++ CarCost/CarCost/ViewReport.m
@@ -7,12 +7,61 @@
 //
 
 #import "ViewReport.h"
+#import "ModelCar.h"
 
 @interface ViewReport ()
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *tableData;
 @end
 
 @implementation ViewReport
+
+
+
+- (NSMutableArray *) tableData
+{
+    if ( !_tableData){
+        _tableData = [[NSMutableArray alloc] init];
+        
+        ModelCar *car = [[ModelCar alloc] init];
+        car.name = @"MY CAR B";
+        [ _tableData addObject:car];
+        
+        ModelCar *cara = [[ModelCar alloc] init];
+        cara.name = @"MY CAR A";
+        [ _tableData addObject:cara];
+        
+    }
+    
+    return _tableData;
+};
+
+#pragma mark - UITableView delegate methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    if ( [[ [self.tableData objectAtIndex:indexPath.row] class] isSubclassOfClass:[ModelCar class]])
+    {
+        ModelCar *model = (ModelCar *)[self.tableData objectAtIndex:indexPath.row];
+        cell.textLabel.text = [[NSString alloc] initWithFormat:@"%@ $ %@", [model name], [model name] ];
+    }
+    
+    return cell;
+}
+
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
