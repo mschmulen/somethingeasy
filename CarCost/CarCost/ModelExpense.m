2a82e3fc5d78fde32e05c299695986ce072ea132
Matt Schmulen <matt.schmulen@gmail.com>
2013-11-10 15:12:47 -0800

    new images

---
+++ CarCost/CarCost/ModelExpense.m
@@ -0,0 +1,13 @@
+//
+//  ModelExpense.m
+//  carcost
+//
+//  Created by Matt Schmulen on 11/10/13.
+//  Copyright (c) 2013 Matt Schmulen. All rights reserved.
+//
+
+#import "ModelExpense.h"
+
+@implementation ModelExpense
+
+@end
