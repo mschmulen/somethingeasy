bd4b8a65e7da8d5fe38053cba27d9ce30120078f
Matt Schmulen <matt.schmulen@gmail.com>
2013-11-17 16:33:00 -0800

    photo update

---
+++ CarCost/CarCost/utility.h
@@ -0,0 +1,19 @@
+//
+//  utility.h
+//  carcost
+//
+//  Created by Matt Schmulen on 11/17/13.
+//  Copyright (c) 2013 Matt Schmulen. All rights reserved.
+//
+
+#ifndef carcost_utility_h
+#define carcost_utility_h
+
+
+#pragma mark - Platform
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define IS_SIMULATOR TARGET_IPHONE_SIMULATOR
+
+
+#endif
